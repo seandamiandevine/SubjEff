e6deeec13b0d0d558a3968d1ca008f8f3bf8f569
seandamiandevine@gmail.com <seandamiandevine@gmail.com>
2021-07-22 16:33:43 -0400

    py task

--- local/subjeff.py
+++ local/subjeff.py
@@ -83,8 +83,25 @@ def runTask(id, sex, age):
     pTarget = 1/3                            # proportion of cues that are target in a block
     
     # initialize instructions 
-    
-    
+    i1 = visual.TextStim(win=win, name='i1',
+        text="Welcome to the study! Press SPACE to navigate through the instructions.", 
+        pos=(0, 0), height=fontH, wrapWidth=30, ori=0, color=textCol)
+        
+    i2 = visual.TextStim(win=win, name='i1',
+        text="Today, we will ask you to play a game called the N-back. The game is simple. \
+You will see a series of letters appear on the screen one at a time.\
+Your job is to say whether the the same letter appeared N steps back or not.", 
+        pos=(0, 0), height=fontH, wrapWidth=30, ori=0, color=textCol)
+
+    i3 = visual.TextStim(win=win, name='i1',
+        text="For example, imagine you saw the following sequences of letters appear (one at a time):\n\n\
+C g C D R \n\n\
+As you can see, the 'C' appears twice in the sequence: once at the start and then again 2 letters later.\
+Your job will be to keep track of the letters as they appear on the screen and respond appropriately when \
+a letter shows up after a certain number of other letters.", 
+        pos=(0, 0), height=fontH, wrapWidth=30, ori=0, color=textCol)
+
+    instsA = [i1, i2, i3]
     
     # initialize trial components
     tClock = core.Clock()
@@ -94,6 +111,13 @@ def runTask(id, sex, age):
     cue = visual.TextStim(win, text="", height=3*fontH, color=textCol, pos=[0, 0])
     
     #--------------------------------------Start Task-----------------------------------------
+    # Instructions 
+    for i in instsA: 
+        i.draw()
+        win.flip()
+        event.waitKeys(keyList = ['space'])
+    
+    # Test trials
     for r in range(numruns):
         combo = list(it.product(Nlev, rewards))
         random.shuffle(combo)
